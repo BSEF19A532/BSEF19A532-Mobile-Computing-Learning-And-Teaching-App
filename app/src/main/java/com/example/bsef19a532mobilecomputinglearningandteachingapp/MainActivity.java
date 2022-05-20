9bbf6aa03e2df23525b492dbbb5bb0a5067f5ad2
BSEF19A532 <bsef19a532@pucit.edu.pk>
2022-05-20 23:20:53 +0500

    Added buttons for the additional activities

--- app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/MainActivity.java
+++ app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.bsef19a532mobilecomputinglearningandteachingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_learn;
+    Button btn_exam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_learn = findViewById(R.id.btn_learn);
+        btn_exam = findViewById(R.id.btn_exam);
+
+        // move to the learning activity
+        btn_learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Learning.class);
+                startActivity(intent);
+            }
+        });
+
+        // move to the exam activity
+        btn_exam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
