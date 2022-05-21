25cbe21ac9000ae1fadc9acc3bfa5c360f6b2fec
BSEF19A532 <bsef19a532@pucit.edu.pk>
2022-05-21 21:27:47 +0500

    Added back button feature

--- app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/Learning.java
+++ app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/Learning.java
@@ -37,15 +37,42 @@ public class Learning extends AppCompatActivity {
         current_index = 0;
 
         img = findViewById(R.id.image_learning);
+        txt = findViewById(R.id.learning_text);
         btn_next = findViewById(R.id.btn_next);
+        btn_prev = findViewById(R.id.btn_prev);
 
+        // next button
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 current_index++;
                 img.setImageResource( images[current_index] );
+                txt.setText( contents[current_index] );
+
+                if ( ! btn_prev.isShown() && current_index > 0 ) {
+                    btn_prev.setVisibility(View.VISIBLE);
+                }
+
                 if ( current_index >= images.length - 1 ) {
                     btn_next.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        // previous button
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_index--;
+                img.setImageResource( images[current_index] );
+                txt.setText( contents[current_index] );
+
+                if ( ! btn_next.isShown() && current_index < images.length - 1 ) {
+                    btn_next.setVisibility(View.VISIBLE);
+                }
+
+                if ( current_index <= 0 ) {
+                    btn_prev.setVisibility(View.INVISIBLE);
                 }
             }
         });
