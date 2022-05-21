bc3daa2c219f86a93b9ed26a9818793d1722f552
BSEF19A532 <bsef19a532@pucit.edu.pk>
2022-05-21 21:04:37 +0500

    Added next image feature

--- app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/Learning.java
+++ app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/Learning.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -33,6 +34,20 @@ public class Learning extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
 
+        current_index = 0;
 
+        img = findViewById(R.id.image_learning);
+        btn_next = findViewById(R.id.btn_next);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                current_index++;
+                img.setImageResource( images[current_index] );
+                if ( current_index >= images.length - 1 ) {
+                    btn_next.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
