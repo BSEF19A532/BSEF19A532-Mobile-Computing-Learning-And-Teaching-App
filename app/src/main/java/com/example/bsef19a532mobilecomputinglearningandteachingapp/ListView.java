b5d82d547fad64f6c7c1365909b7c45c03fc4a89
BSEF19A532 <bsef19a532@pucit.edu.pk>
2022-05-28 19:37:15 +0500

    Created List View Activity

---
+++ app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/ListView.java
@@ -0,0 +1,14 @@
+package com.example.bsef19a532mobilecomputinglearningandteachingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
