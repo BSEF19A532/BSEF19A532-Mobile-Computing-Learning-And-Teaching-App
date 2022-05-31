62d622c112d9ca4e97dd44cb3b6228bb23e41a42
BSEF19A532 <bsef19a532@pucit.edu.pk>
2022-05-31 18:23:15 +0500

    Adapted the array list

--- app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/ListViewActivity.java
+++ app/src/main/java/com/example/bsef19a532mobilecomputinglearningandteachingapp/ListViewActivity.java
@@ -3,11 +3,16 @@ package com.example.bsef19a532mobilecomputinglearningandteachingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
 
     ListView lst_view;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,7 @@ public class ListViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_view);
 
         lst_view = findViewById(R.id.lst_view);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
     }
 }
